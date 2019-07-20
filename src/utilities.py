f422c81e04d301149a07964d231484ac253b2820
skconan <supakit.kr@gmail.com>
2019-07-20 21:05:39 +0700

    done compressed img

--- src/utilities.py
+++ src/utilities.py
@@ -91,7 +91,7 @@ def publish_result(img, type, topic_name):
         type = "gray"
 
     pub = rospy.Publisher(
-        str(topic_name), CompressedImage, queue_size=10)
+        str(topic_name)+"/compressed", CompressedImage, queue_size=10)
     
     msg = CompressedImage()
     msg.header.stamp = rospy.Time.now()
