e2db821a4c857cedc4b48658c9973fa8e0b8b336
skconan <supakit.kr@gmail.com>
2019-07-21 02:14:34 +0700

    add param

--- src/bg_sub_by_kmean.py
+++ src/bg_sub_by_kmean.py
@@ -37,7 +37,7 @@ def kmean(img, k):
     
     return result
 
-def bg_subtraction(img, bg_k=1, fg_k=3):
+def bg_subtraction(img, bg_k=1, fg_k=3, mode='neg'):
     hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
     gray,_,_ = cv.split(hsv)
 
@@ -47,18 +47,24 @@ def bg_subtraction(img, bg_k=1, fg_k=3):
  
 
     sub_sign = np.int16(fg) - np.int16(bg)
-    sub_neg = np.clip(sub_sign.copy(),sub_sign.copy().min(),0)
-    sub_neg = normalize(sub_neg)
     
-    _, obj_neg = cv.threshold(
-        sub_neg, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU
-    )
+    if mode == 'neg':
+        sub_neg = np.clip(sub_sign.copy(),sub_sign.copy().min(),0)
+        sub_neg = normalize(sub_neg)
+        _, result = cv.threshold(
+            sub_neg, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU
+        )
+    else:
+        sub_pos = np.clip(sub_sign.copy(),0,sub_sign.copy().max())
+        sub_pos = normalize(sub_pos)
+        _, obj_pos = cv.threshold(
+            result, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU
+        )
 
     # time_duration = rospy.Time.now()-start_time
     # print(time_duration.to_sec())
-    # publish_result(obj_neg, "/path_obj_neg")
 
-    return obj_neg
+    return result
 
 # if __name__ == "__main__":
 #     rospy.init_node("test_bg_sub_kmean")
