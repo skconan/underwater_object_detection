7f80d73310ce51d8d9080d8de2692be1e73e7107
skconan <supakit.kr@gmail.com>
2019-07-20 18:53:18 +0700

    manage gpu

--- src/semantic_segmentation.py
+++ src/semantic_segmentation.py
@@ -12,7 +12,22 @@ from utilities import *
 import os
 from sensor_msgs.msg import CompressedImage
 from keras.models import load_model
-
+## extra imports to set GPU options
+import tensorflow as tf
+from keras import backend as k
+ 
+###################################
+# TensorFlow wizardry
+config = tf.ConfigProto()
+ 
+# Don't pre-allocate memory; allocate as-needed
+config.gpu_options.allow_growth = True
+ 
+# Only allow a total of half the GPU memory to be allocated
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+ 
+# Create a session with the above options specified.
+k.tensorflow_backend.set_session(tf.Session(config=config))
 
 model = None
 image = None
